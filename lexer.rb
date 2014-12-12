d61869464e2bae5106135af4782530b1d3e854fb
sshao <sshao@users.noreply.github.com>
2014-12-12 15:22:04 -0600

    first attempt at a lexer
    based off txus' implementation: https://github.com/txus/brainfuck

---
+++ lexer.rb
@@ -0,0 +1,41 @@
+class Brainfuck
+  class Lexer
+    COMMANDS = %w(> < + - . , [ ])
+
+    def initialize(code)
+      @sexp = {expr: []}
+      @sexp_stack = [@sexp]
+      @code = code
+    end
+
+    def lex
+      clean
+      @code.each_char do |cmd|
+        rule(cmd)
+      end
+      @sexp
+    end
+
+    private
+    def clean
+      @code = @code.chars.select { |c| COMMANDS.include? c }.join
+    end
+
+    def rule(cmd)
+      case cmd
+      when ">" then @sexp_stack.last[:expr] << {ptr_inc: ">"}
+      when "<" then @sexp_stack.last[:expr] << {ptr_dec: "<"}
+      when "+" then @sexp_stack.last[:expr] << {inc: "+"}
+      when "-" then @sexp_stack.last[:expr] << {dec: "-"}
+      when "." then @sexp_stack.last[:expr] << {puts: "."}
+      when "," then @sexp_stack.last[:expr] << {gets: ","}
+      when "[" then
+        @sexp_stack.push({expr: []})
+      when "]" then
+        iter = @sexp_stack.pop
+        @sexp_stack.last[:expr] << {iteration: iter}
+      else raise "#{cmd} is not a valid command"
+      end
+    end
+  end
+end
