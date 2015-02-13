5c11e56514fc55f6d9ff74d978bace9b3c923ce5
sshao <sshao@users.noreply.github.com>
2015-02-13 12:15:24 -0600

    implemented lexer tests + more error checking

--- lexer.rb
+++ lexer.rb
@@ -1,3 +1,5 @@
+class LexerError < StandardError; end
+
 class Brainfuck
   class Lexer
     COMMANDS = %w(> < + - . , [ ])
@@ -9,18 +11,18 @@ class Brainfuck
 
     def lex
       clean
-      @code.each_char do |cmd|
-        rule(cmd)
+      @code.each_char.with_index do |cmd, i|
+        rule(cmd, i)
       end
       @sexp_stack
     end
 
     private
     def clean
-      @code = @code.chars.select { |c| COMMANDS.include? c }.join
+      @code = @code.gsub(/\s+/, "")
     end
 
-    def rule(cmd)
+    def rule(cmd, i)
       case cmd
       when ">" then @sexp_stack.last[:expr] << :ptr_inc
       when "<" then @sexp_stack.last[:expr] << :ptr_dec
@@ -29,12 +31,46 @@ class Brainfuck
       when "." then @sexp_stack.last[:expr] << :puts
       when "," then @sexp_stack.last[:expr] << :gets
       when "[" then
+        raise LexerError, "missing matching brace" if !matching_brace?(i)
         @sexp_stack.push({expr: []})
       when "]" then
+        raise LexerError, "missing matching brace" if !matching_brace?(i, :-)
         iter = @sexp_stack.pop
         @sexp_stack.last[:expr] << {iteration: [iter]}
-      else raise "#{cmd} is not a valid command"
+      else
+        raise LexerError, "#{cmd} is not a valid command"
       end
+    end
+
+    def within_bounds?(index)
+      return index >= 0 && index < @code.length
+    end
+
+    def matching_brace?(start, dir = :+)
+      br_stack = []
+      cur = start
+      while within_bounds?(cur)
+        cmd = @code[cur]
+
+        unless cmd == "[" or cmd == "]"
+          cur = cur.send(dir, 1)
+          next
+        end
+
+        if dir == :-
+          br_stack.push(cmd) if cmd == "]"
+          br_stack.pop if cmd == "["
+        else
+          br_stack.push(cmd) if cmd == "["
+          br_stack.pop if cmd == "]"
+        end
+
+        break if br_stack.size == 0
+
+        cur = cur.send(dir, 1)
+      end
+
+      return within_bounds?(cur)
     end
   end
 end
---
+++ test/lexer_test.rb
@@ -0,0 +1,42 @@
+require_relative 'test_helper'
+
+class TestLexer < MiniTest::Test
+
+  def test_invalid_characters
+    err = assert_raises LexerError do
+      Brainfuck::Lexer.new(">a>s>d>f").lex
+    end
+    assert_match /not a valid command/, err.message
+  end
+
+  def test_unmatched_iter_start
+    err = assert_raises LexerError do
+      Brainfuck::Lexer.new("[+++").lex
+    end
+    assert_match /missing matching brace/, err.message
+  end
+
+  def test_unmatched_iter_end
+    err = assert_raises LexerError do
+      Brainfuck::Lexer.new("+++]").lex
+    end
+    assert_match /missing matching brace/, err.message
+  end
+
+  def test_unmatched_iter_nested
+    err = assert_raises LexerError do
+      Brainfuck::Lexer.new("[[+]]+]").lex
+    end
+    assert_match /missing matching brace/, err.message
+  end
+
+  def test_strips_whitespace
+    expected = [{expr: [:ptr_inc, :ptr_inc]}]
+
+    inputs = [" > > ", "\t>\t>\t", "\n>\n>\n"]
+
+    inputs.each do |input|
+      assert_equal expected, Brainfuck::Lexer.new(input).lex
+    end
+  end
+end
